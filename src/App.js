ba9e18e6a38a6aa63a477e40f3e20c29290a0157
Rom0105 <savosko.photo@gmail.com>
2021-10-01 16:55:04 +0300

    правки

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Statistics from './components/Statistics';
 import statisticalData from './statistical-data.json';
 import FriendList from './components/FriendList';
 import friends from './friends.json';
+import transactions from './transactions.json';
+import TransactionHistory from './components/TransactionHistory';
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </>
   );
 }
---
+++ src/components/TransactionHistory.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+function TransactionHistory({ items }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default TransactionHistory;
+
+TransactionHistory.propTypes = {
+  id: PropTypes.string,
+  type: PropTypes.string,
+  amount: PropTypes.string,
+  currency: PropTypes.string,
+};
